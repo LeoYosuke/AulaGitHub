fd42aef6f645930431502d579a03898c3eea5d8f
Leonardo Yosuke Konichi <135863260+LeoYosuke@users.noreply.github.com>
2023-06-07 14:23:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaGITHUB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ola! esse é eu projeto");
+            Console.WriteLine("Ele é um teste para o GitHub");
+            Console.WriteLine("Nada de novo por aqui....");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
